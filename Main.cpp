d84572c75cb2a0519cc0599a309d127722e6089f
Caio Bertti <caiovbertti@gmail.com>
2024-09-25 14:34:58 -0300

    Just some comentaries on the previous code

--- Main.cpp
+++ Main.cpp
@@ -4,14 +4,18 @@
 
 int main()
 {
+    // Marca o início do GLFW
     glfwInit();
 
+    // Especifica as versões máximas e mínimas permitidas do OpenGL
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-    GLFWwindow* window = glfwCreateWindow(800, 800, "Abobrinha", NULL, NULL);
+    // Criando a janela com largura, altura, nome e fullscreen
+    GLFWwindow* window = glfwCreateWindow(800, 800, "Tutorial0", NULL, NULL);
 
+    // Se a janela não foi criada
     if (window == NULL)
     {
         std::cout << "Failed to create Window" << std::endl;
@@ -19,21 +23,27 @@ int main()
         return -1;
     }
 
+    // Define o contexto atual da janela
     glfwMakeContextCurrent(window);
 
+    // Inicializa o OpenGL
     gladLoadGL();
 
-    glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
+    // Define a cor do buffer
+    glClearColor(0.0f, 0.55f, 0.12f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT);
     glfwSwapBuffers(window);
 
+    // Define as dimensões da janela
     glViewport(0, 0, 800, 800);
 
+    // Mantém a janela aberta até que seja fechada
     while (!glfwWindowShouldClose(window))
     {
         glfwPollEvents();
     }
 
+    // Fecha a janela e termina o GLFW
     glfwDestroyWindow(window);
     glfwTerminate();
 
